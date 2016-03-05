2724782f7a0f3ddd6c67ec92fb8b3ccc5742c22d
Cody Cates <Cody Cates>
2016-03-05 11:45:57 -0500

    Updated the app.js and package.json files with additional software components needed for site development

--- sportsApp/app.js
+++ sportsApp/app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+mongoose.connect('mongodb://localhost/sportsApp')
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
