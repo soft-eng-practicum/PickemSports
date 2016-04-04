cdd3d938624ced86c3bbfe48f1ddae5e172f319d
[aprintz3] <aprintz@ggc.edu>
2016-04-04 15:01:20 -0400

    more attempts at karma and travis

--- sportsApp/karma.conf.js
+++ sportsApp/karma.conf.js
@@ -15,6 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/angular-moment/angular-moment.js',
+      'public/javascripts/controllers/*.js',
     ],
 
 
