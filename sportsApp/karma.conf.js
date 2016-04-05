862cc1986e0e0667d8626cac86802fc67a8f19e2
[aprintz3] <aprintz@ggc.edu>
2016-04-05 17:17:39 -0400

    testing with karma

--- sportsApp/karma.conf.js
+++ sportsApp/karma.conf.js
@@ -15,8 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/angular-moment/angular-moment.js',
-      'public/javascripts/controllers/*.js',
+      'spec/*.js',
     ],
 
 
