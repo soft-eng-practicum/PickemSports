00b523cd5b646b1be6f3b5f4048092c448fd9dd7
[aprintz3] <aprintz@ggc.edu>
2016-04-06 23:30:20 -0400

    testing

--- sportsApp/karma.conf.js
+++ sportsApp/karma.conf.js
@@ -15,8 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'public/javascripts/*.js',
-        'public/javascripts/controllers/*.js'
         'spec/*.js',
     ],
 
