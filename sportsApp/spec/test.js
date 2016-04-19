5b9c5a16f5106362f646b150f977b511c2c65b14
[aprintz3] <aprintz@ggc.edu>
2016-04-19 14:59:08 -0400

    more work on unit test

--- sportsApp/spec/test.js
+++ sportsApp/spec/test.js
@@ -3,3 +3,28 @@ describe('true', function() {
     expect(true).toBeTruthy();
   });
 });
+
+
+describe ('auth fac', function() {
+  var authService;
+  var window = null;
+
+  beforeEach(function() {
+    module('sportsApp.services.auth');
+
+    inject(function($injector) {
+      authService = $injector.get('authService');
+    });
+
+    window = { localStorage: {'sportsApp-token': "hello"} };
+  });
+
+  it('should save token', function() {
+    expect(window.localStorage['sportsApp-token']).toBe('hello');
+    authService.saveToken('hi');
+    expect(authService.getToken()).toBe('hi');
+  });
+
+});
+
+
